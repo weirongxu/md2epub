7df8e5d9cccf7e2268c3e4c517943b99ad4c4800
Weirong Xu <weirongxu.raidou@gmail.com>
2023-06-21 19:45:15 +0800

    fix: init

--- src/init.ts
+++ src/init.ts
@@ -1,8 +1,9 @@
 import fs from 'fs'
 import path from 'path'
 export async function initBy(lang: string | undefined) {
-  const filename = lang ? `epub-builder.${lang}.yaml` : 'epub-builder.yaml'
-  const tplPath = path.join(__dirname, '../template', filename)
+  const filename = 'epub-builder.yaml'
+  const sourceFilename = lang ? `epub-builder.${lang}.yaml` : filename
+  const tplPath = path.join(__dirname, '../template', sourceFilename)
   const content = fs.readFileSync(tplPath)
   fs.writeFileSync(filename, content)
 }
